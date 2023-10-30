9489699985a842bcb395dcb190503a5f1d01da2d
biskitsx <112327677+biskitsx@users.noreply.github.com>
2023-10-30 20:22:27 +0700

    custom tailwind and add daisyui

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -1,20 +1,25 @@
+import daisyui from 'daisyui'
+import { Days_One } from 'next/font/google'
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    content: [
+        './pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './components/**/*.{js,ts,jsx,tsx,mdx}',
+        './app/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    theme: {
+        extend: {
+            width: {
+                '128': '32rem',
+                '540': '540px',
+                '720': '720px',
+                '960': '960px',
+                '1140': '1140px',
+                '1320': '1320px',
+            }
+        },
     },
-  },
-  plugins: [],
+    plugins: [daisyui],
 }
 export default config
