7f56f08a1eb46a7d7d4a566d38057db257461cb0
biskitsx <112327677+biskitsx@users.noreply.github.com>
2023-11-01 00:05:15 +0700

    change img to Image component

--- client/components/ProductCard.tsx
+++ client/components/ProductCard.tsx
@@ -1,4 +1,7 @@
+
 import React from 'react'
+import Rating from './Rating'
+import Image from 'next/image'
 
 interface ProductCardProps {
     productImg: string,
@@ -10,9 +13,10 @@ interface ProductCardProps {
 function ProductCard({ productImg, productName, productPrice, productStar }: ProductCardProps) {
     return (
         <div className="card  rounded-none card-compact">
-            <figure><img src={productImg} alt="Shoes" className='w-full' />
+            <figure><Image src={productImg} alt="Shoes" className='w-full' width={1000} height={1000} />
             </figure>
             <div className="card-body text-center">
+                <Rating rating={Math.floor(Math.random() * 10) % 5} />
                 <h2 className="font-medium">{productName}</h2>
                 <h2 className=""><span className='line-through text-zinc-400'>${productPrice + 20}.00</span><span className='font-semibold'> ${productPrice}.00</span></h2>
             </div>
