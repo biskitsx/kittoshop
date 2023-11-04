231e1f811b073adbc9f42b6109f5104be08aa8ea
biskitsx <112327677+biskitsx@users.noreply.github.com>
2023-11-04 22:05:22 +0700

    resolve rating

--- client/components/Rating.tsx
+++ client/components/Rating.tsx
@@ -4,7 +4,7 @@ import { Star } from '@mui/icons-material';
 function Rating({ rating }: { rating: number }) {
     return (
         // <div>
-        <div className='flex gap-1 text-sm justify-center'>
+        <div className=' text-sm '>
             <Star className={rating >= 1 ? 'text-yellow-400' : 'text-gray-400'} sx={{ fontSize: 18 }} />
             <Star className={rating >= 2 ? 'text-yellow-400' : 'text-gray-400'} sx={{ fontSize: 18 }} />
             <Star className={rating >= 3 ? 'text-yellow-400' : 'text-gray-400'} sx={{ fontSize: 18 }} />
