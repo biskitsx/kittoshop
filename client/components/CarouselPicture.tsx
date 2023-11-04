e795c156e0c878447f9b32c075a954247de9a435
biskitsx <112327677+biskitsx@users.noreply.github.com>
2023-11-04 22:03:29 +0700

    update carousel component

--- client/components/CarouselPicture.tsx
+++ client/components/CarouselPicture.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import Carousel from 'nuka-carousel'
 import React from 'react'
 
@@ -10,8 +11,17 @@ function CarouselPicture() {
         // className='h-5/6'
         >
             {/* <img src="https://jewelleryishi.myshopify.com/cdn/shop/files/Rectangle_1510.png?v=1692178083" alt="" /> */}
-            < img src="https://jewelleryishi.myshopify.com/cdn/shop/files/slider2.png?v=1692851555" alt="" />
-            <img src="https://jewelleryishi.myshopify.com/cdn/shop/files/Rectangle_2.png?v=1690284812" alt="" className='object-fill' />
+
+            <div className='relative'>
+                < Image src="https://jewelleryishi.myshopify.com/cdn/shop/files/slider2.png?v=1692851555" alt="123" width={4000} height={4000} className='w-full h-full' />
+                <div className="absolute left-32 z-2 bottom-1/4 text-white" >
+                    <p className='text-xl'>End Of Season</p>
+                    <p className='text-5xl'>New Arrival</p>
+                    <button className='white-button'>SHOP NOW</button>
+                </div>
+            </div>
+            < Image src="https://jewelleryishi.myshopify.com/cdn/shop/files/Rectangle_2.png?v=1690284812" alt="123" width={4000} height={4000} className='w-full h-full' />
+            {/* <img src="https://jewelleryishi.myshopify.com/cdn/shop/files/Rectangle_2.png?v=1690284812" alt="" className='object-fill' /> */}
             {/* <img src="https://cdn.mercular.com/images/homepage/sections/8/spaces/11/1698242117666_HP-Banner.jpg" alt="" className='object-fill' />
             <img src="https://cdn.mercular.com/images/homepage/sections/8/spaces/11/1698242117666_HP-Banner.jpg" alt="" className='object-fill' /> */}
         </Carousel >
