fe33c06a8a72640b98759f2f78f5fb7e7b457635
biskitsx <112327677+biskitsx@users.noreply.github.com>
2023-11-05 22:15:15 +0700

    fix size of coundown component

--- client/components/Countdown.tsx
+++ client/components/Countdown.tsx
@@ -2,27 +2,27 @@ import React from 'react'
 
 function Countdown() {
     return (
-        <div className="grid grid-flow-col gap-5 text-center auto-cols-max">
+        <div className="grid grid-flow-col gap-5 text-center auto-cols-max ">
             <div className="flex flex-col">
-                <span className="countdown font-mono text-5xl">
+                <span className="countdown font-mono text-lg">
                     <span style={{ "--value": 15 }}></span>
                 </span>
                 days
             </div>
             <div className="flex flex-col">
-                <span className="countdown font-mono text-5xl">
+                <span className="countdown font-mono text-lg">
                     <span style={{ "--value": 10 }}></span>
                 </span>
                 hours
             </div>
             <div className="flex flex-col">
-                <span className="countdown font-mono text-5xl">
+                <span className="countdown font-mono text-lg">
                     <span style={{ "--value": 24 }}></span>
                 </span>
                 min
             </div>
             <div className="flex flex-col">
-                <span className="countdown font-mono text-5xl">
+                <span className="countdown font-mono text-lg">
                     <span style={{ "--value": 48 }}></span>
                 </span>
                 sec
