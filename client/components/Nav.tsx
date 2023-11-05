11a2b561ac4720d434a5775d5f4e2ee96abecd10
biskitsx <112327677+biskitsx@users.noreply.github.com>
2023-11-05 22:16:56 +0700

    add cart to nav

--- client/components/Nav.tsx
+++ client/components/Nav.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import Drawer from './Drawer';
-
+import { useDispatch, useSelector } from 'react-redux'
+import { selectCart } from '@/store/cartSlice';
 function Nav() {
+    const cart = useSelector(selectCart)
     return (
         <div className='flex flex-col shadow-md sticky top-0 z-10'>
             <div className='w-full bg-white'>
@@ -35,7 +37,7 @@ function Nav() {
                                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
                                         </svg>
-                                        <span className="badge badge-sm indicator-item">8</span>
+                                        <span className="badge badge-sm indicator-item">{cart.length}</span>
                                     </div>
                                 </label>
                                 <Drawer />
