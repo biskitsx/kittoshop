3a817b266966a7d0392602f9bee273ada9c2aa5b
biskitsx <112327677+biskitsx@users.noreply.github.com>
2023-11-05 22:18:03 +0700

    add redux

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,7 +1,10 @@
+import { wrapper } from '@/store/store'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Fira_Sans } from 'next/font/google/index'
 
-export default function App({ Component, pageProps }: AppProps) {
+function App({ Component, pageProps }: AppProps) {
     return <Component {...pageProps} />
 }
+
+export default wrapper.withRedux(App)
