674c928cca43d8fad4cad23da07ca152e705b0e9
biskitsx <112327677+biskitsx@users.noreply.github.com>
2023-11-07 23:28:12 +0700

    add collection

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -6,6 +6,7 @@ import CarouselPicture from '@/components/CarouselPicture'
 import NewArrival from '@/components/NewArrival'
 import WhyMe from '@/components/WhyMe'
 import Countdown from '@/components/Countdown'
+import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -36,17 +37,31 @@ export default function Home() {
                 </div>
             </div>
             <div className='py-12'>
-                <div className="container flex-col gap-12 justify-between items-center">
-                    <h1 className="text-2xl   text-gray-800">
-                        This Week's Highlight
+                <div className="container flex flex-col gap-12 justify-between items-center">
+                    <h1 className="text-4xl text-center  text-gray-800">
+                        Collection List
                     </h1>
-                    <Image src={'https://jewelleryishi.myshopify.com/cdn/shop/files/Rectangle_60_1.png?v=1690363154'} alt='collection' width={1000} height={1000} className='w-full' />
+                    <div className='grid grid-cols-2 md:grid-cols-4 gap-4'>
+                        <Link href={"/collection/fashion"} className='flex flex-col gap-2'>
+                            <Image src={'https://jewelleryishi.myshopify.com/cdn/shop/files/img_bdd986bb-b532-404e-8b63-515ec732be25.png?v=1690373191'} alt='collection' width={1000} height={1000} className='w-full hover:scale-105 ease-in duration-100' />
+                            <h3 className='text-center text-xl text-gray-800'>Fashion</h3>
+                        </Link>
+                        <Link href={"/collections/kid"} className='flex flex-col gap-2'>
+                            <Image src={'https://jewelleryishi.myshopify.com/cdn/shop/files/img_2.png?v=1690373191'} alt='collection' width={1000} height={1000} className='w-full hover:scale-105 ease-in duration-100' />
+                            <h3 className='text-center text-xl text-gray-800'>Kids</h3>
+                        </Link>
+                        <Link href={"/collections/men"} className='flex flex-col gap-2'>
+                            <Image src={'https://jewelleryishi.myshopify.com/cdn/shop/files/Rectangle_1468-1.png?v=1690373191'} alt='collection' width={1000} height={1000} className='w-full hover:scale-105 ease-in duration-100' />
+                            <h3 className='text-center text-xl text-gray-800'>Men</h3>
+                        </Link>
+                        <Link href={"/collections/women"} className='flex flex-col gap-2'>
+                            <Image src={'https://jewelleryishi.myshopify.com/cdn/shop/files/Rectangle_1468.png?v=1690373191'} alt='collection' width={1000} height={1000} className='w-full hover:scale-105 ease-in duration-100' />
+                            <h3 className='text-center text-xl text-gray-800'>Women</h3>
+                        </Link>
+                    </div>
                 </div>
 
-                {/* <button className='black-button'>get 20% off</button> */}
-                {/* <button className=''>get 20% off</button> */}
             </div>
-            {/* </div > */}
         </>
     )
 }
