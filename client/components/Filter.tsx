41a99bd80382c71e78fc9a3a5ff9ca2215fc6b1a
biskitsx <112327677+biskitsx@users.noreply.github.com>
2023-11-07 18:33:47 +0700

    handle filter product

--- client/components/Filter.tsx
+++ client/components/Filter.tsx
@@ -4,6 +4,7 @@ import { useDispatch } from 'react-redux';
 import { FilterProps, PriceDTO, SizeDTO, AvailabilityDTO } from './Filter.dto'
 import axios from 'axios';
 
+
 function Filter({ loading, setLoading, type }: FilterProps) {
     const [availabilityFilters, setAvailabilityFilters] = useState<AvailabilityDTO>({
         inStock: false,
@@ -54,7 +55,7 @@ function Filter({ loading, setLoading, type }: FilterProps) {
         }));
     };
 
-    const resetFilters = () => {
+    const resetFilters = async () => {
         setAvailabilityFilters({
             inStock: false,
             outOfStock: false,
@@ -69,34 +70,66 @@ function Filter({ loading, setLoading, type }: FilterProps) {
             M: false,
             L: false,
         });
+        await getProduct()
+    }
+    function delay(time: number) {
+        return new Promise(resolve => setTimeout(resolve, time));
     }
 
-    // const handleFilter = async () => {
-    //     setLoading(true)
-    //     try {
-    //         const res = await axios.get('http://localhost:3001/products', {})
-    //     }
-    // }
-    // console.log({ availabilityFilters, priceFilters, colorFilters, sizeFilters })
+    const handleFilter = async () => {
+        setLoading(true)
+        try {
+            let colorsString: string | null = colorFilters.join(',')
+            if (colorsString === "") {
+                colorsString = null
+            }
+            // let x = Object.keys(sizeFilters)
+            let keys = Object.keys(sizeFilters)
+            let sizesString: string | null = (keys.filter((key) => sizeFilters[key as keyof SizeDTO])).join(',')
+            if (sizesString === "") {
+                sizesString = null
+            }
+            const res = await axios.get('http://localhost:3001/products', {
+                params: {
+                    type: type,
+                    // availability: availabilityFilters,
+                    // price: priceFilters,
+                    color: colorsString,
+                    size: sizesString,
+                    lowest_price: priceFilters.from,
+                    highest_price: priceFilters.to,
+                }
+            })
+            dispatch(setProduct(res.data))
+        } catch (error) {
+            console.log(error)
+        }
+        delay(100).then(() => setLoading(false))
+        // setLoading(false)
+    }
+
+    const getProduct = async () => {
+        setLoading(true)
+        try {
+            const res = await axios.get('http://localhost:3001/products', {
+                params: {
+                    type: type
+                }
+            })
+            dispatch(setProduct(res.data))
+        } catch (error) {
+            console.log(error)
+        }
+        setLoading(false)
+    }
     const dispatch = useDispatch()
     useEffect(() => {
-        const getProduct = async () => {
-            setLoading(true)
-            try {
-                const res = await axios.get('http://localhost:3001/products', {
-                    params: {
-                        type: type
-                    }
-                })
-                dispatch(setProduct(res.data))
-            } catch (error) {
-                console.log(error)
-            }
-            setLoading(false)
-        }
         getProduct()
     }, [])
 
+    useEffect(() => {
+        handleFilter()
+    }, [priceFilters, colorFilters, sizeFilters])
 
     return (
         <aside className='flex-col '>
@@ -114,7 +147,6 @@ function Filter({ loading, setLoading, type }: FilterProps) {
                             <div className='flex items-center gap-2'>
                                 <input type='checkbox' onChange={() => handleAvailabilityChange("inStock")} checked={availabilityFilters.inStock} />
                                 <label>In stock</label>
-
                             </div>
                             <div className='flex items-center gap-2'>
                                 <input type='checkbox' onChange={() => handleAvailabilityChange("outOfStock")} checked={availabilityFilters.outOfStock} />
@@ -128,24 +160,24 @@ function Filter({ loading, setLoading, type }: FilterProps) {
                     <div className='grid grid-cols-2 w-full gap-2'>
                         <div className='flex items-center gap-2'>
                             <label className='text-sm' >$</label>
-                            <input type='number' className='border-gray-300 border w-full pl-1' placeholder='FROM' name='from' onChange={handlePriceChange} value={priceFilters.from ? priceFilters.from : undefined} />
+                            <input type='number' className='border-gray-300 border w-full pl-1' placeholder='FROM' name='from' onChange={handlePriceChange} value={priceFilters.from ? priceFilters.from : ""} min={0} />
                         </div>
                         <div className='flex items-center gap-2'>
                             <label className='text-sm'>$</label>
-                            <input type='number' className='border-gray-300 border w-full pl-1' placeholder='TO' name='to' onChange={handlePriceChange} value={priceFilters.to ? priceFilters.to : undefined} />
+                            <input type='number' className='border-gray-300 border w-full pl-1' placeholder='TO' name='to' onChange={handlePriceChange} value={priceFilters.to ? priceFilters.to : ""} min={priceFilters.from ? priceFilters.from : 0} />
                         </div>
                     </div>
                 </div>
                 <div className='p-4 flex-col gap-1 flex border-b border-gray-300'>
                     <p className='text-sm font-bold'>Colors</p>
                     <div className='flex flex-wrap gap-3'>
-                        <button className={colorFilters.includes('black') ? 'w-8 h-8 shadow-md border border-gray-400 bg-gray-700' : 'w-8 h-8 shadow-md bg-gray-700'} onClick={() => { handleColorChange("black") }}></button>
-                        <button className={colorFilters.includes('blue') ? 'w-8 h-8 shadow-md bg-cyan-500 border border-gray-400' : 'w-8 h-8 shadow-md bg-cyan-500'} onClick={() => { handleColorChange("blue") }}></button>
-                        <button className={colorFilters.includes('white') ? 'w-8 h-8 shadow-md bg-white-700 border border-gray-400' : 'w-8 h-8 shadow-md bg-white-700'} onClick={() => { handleColorChange("white") }}></button>
-                        <button className={colorFilters.includes('gray') ? 'w-8 h-8 shadow-md bg-gray-600 border border-gray-400' : 'w-8 h-8 shadow-md bg-gray-600'} onClick={() => { handleColorChange("gray") }}></button>
-                        <button className={colorFilters.includes('pink') ? 'w-8 h-8 shadow-md bg-pink-300 border border-gray-400' : 'w-8 h-8 shadow-md bg-pink-300'} onClick={() => { handleColorChange("pink") }}></button>
-                        <button className={colorFilters.includes('purple') ? 'w-8 h-8 shadow-md bg-purple-300 border border-gray-400' : 'w-8 h-8 shadow-md bg-purple-300'} onClick={() => { handleColorChange("purple") }}></button>
-                        <button className={colorFilters.includes('red') ? 'w-8 h-8 shadow-md bg-red-400 border border-gray-400' : 'w-8 h-8 shadow-md bg-red-400'} onClick={() => { handleColorChange("red") }}></button>
+                        <button className={colorFilters.includes('black') ? 'color-button  bg-gray-700 color-button-active' : 'color-button bg-gray-700'} onClick={() => { handleColorChange("black") }}></button>
+                        <button className={colorFilters.includes('blue') ? 'color-button bg-cyan-500 color-button-active' : 'color-button bg-cyan-500'} onClick={() => { handleColorChange("blue") }}></button>
+                        <button className={colorFilters.includes('white') ? 'color-button bg-white-700 color-button-active' : 'color-button bg-white-700'} onClick={() => { handleColorChange("white") }}></button>
+                        <button className={colorFilters.includes('gray') ? 'color-button bg-gray-600 color-button-active' : 'color-button bg-gray-600'} onClick={() => { handleColorChange("gray") }}></button>
+                        <button className={colorFilters.includes('pink') ? 'color-button bg-pink-300 color-button-active' : 'color-button bg-pink-300'} onClick={() => { handleColorChange("pink") }}></button>
+                        <button className={colorFilters.includes('purple') ? 'color-button bg-purple-300 color-button-active' : 'color-button bg-purple-300'} onClick={() => { handleColorChange("purple") }}></button>
+                        <button className={colorFilters.includes('red') ? 'color-button bg-red-400 color-button-active' : 'color-button bg-red-400'} onClick={() => { handleColorChange("red") }}></button>
                     </div>
                 </div>
                 <div className='p-4 flex-col gap-1 flex border-b border-gray-300'>
