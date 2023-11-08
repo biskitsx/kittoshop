bcb9d44091fc44e173cc1ee5313532514f660a82
biskitsx <112327677+biskitsx@users.noreply.github.com>
2023-11-08 13:53:15 +0700

    add title

--- client/pages/_document.tsx
+++ client/pages/_document.tsx
@@ -4,6 +4,7 @@ export default function Document() {
     return (
         <Html lang="en">
             <Head>
+                <title>KITTO SHOP</title>
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 {/* <link rel="preconnect" href="https://fonts.gstatic.com" CrossOrigin /> */}
                 <link href="https://fonts.googleapis.com/css2?family=Fira+Sans:wght@100;200;300;400;500;600;700;800;900&family=Kanit:wght@300;400;500;600;700&family=Poppins:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
