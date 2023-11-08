52635bb92987bf5e8c47b2fe84a6b90c701c1216
biskitsx <112327677+biskitsx@users.noreply.github.com>
2023-11-08 12:08:09 +0700

    add css

--- client/pages/collections/index.tsx
+++ client/pages/collections/index.tsx
@@ -12,7 +12,7 @@ const collections = () => {
             <Nav />
             <div className="container py-12 flex flex-col items-center justify-center gap-8 tracking-wider">
                 <h1 className='text-3xl font-semibold text-zinc-700'>Collection</h1>
-                <div className='grid grid-cols-4 gap-4'>
+                <div className='grid grid-cols-2 md:grid-cols-4 gap-4'>
                     <Collections collectionName='Fashion' collectionImg='https://jewelleryishi.myshopify.com/cdn/shop/files/21_720x.png?v=1690518940' />
                     <Collections collectionName='Kid' collectionImg='https://jewelleryishi.myshopify.com/cdn/shop/files/18_ac04a79f-0968-4337-b197-3125e1aad9c0_720x.png?v=1690355811' />
                     <Collections collectionName='Women' collectionImg='https://jewelleryishi.myshopify.com/cdn/shop/files/20_98aa8791-ba1a-47aa-9b0e-4c78c4c69734_533x.png?v=1690355731' />
