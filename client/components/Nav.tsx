dc6d314b835aab6566e8411437438e1318887c5d
biskitsx <112327677+biskitsx@users.noreply.github.com>
2023-11-08 12:09:44 +0700

    remove uncessary

--- client/components/Nav.tsx
+++ client/components/Nav.tsx
@@ -3,6 +3,7 @@ import Drawer from './Drawer';
 import { useDispatch, useSelector } from 'react-redux'
 import { selectCart } from '@/store/cartSlice';
 import Link from 'next/link';
+
 function Nav() {
     const cart = useSelector(selectCart)
     return (
@@ -10,18 +11,6 @@ function Nav() {
             <div className='w-full bg-white'>
                 <div className="navbar bg-base-100 container">
                     <div className="navbar-start">
-                        {/* <div className="dropdown">
-                            <label tabIndex={0} className="btn btn-ghost btn-circle">
-                                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" />
-                                </svg>
-                            </label>
-                            <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                                <li><a>Homepage</a></li>
-                                <li><a>Portfolio</a></li>
-                                <li><a>About</a></li>
-                            </ul>
-                        </div> */}
                         <Link className="btn btn-ghost normal-case text-2xl tracking-widest" href='/'>KITTOSHOP</Link>
                     </div>
                     <div className="navbar-center  gap-3 hidden md:flex md:text-md text-zinc-500">
@@ -49,14 +38,14 @@ function Nav() {
                                 <Drawer />
                             </div>
                         </div>
-                        <button className="btn btn-ghost btn-circle">
+                        {/* <button className="btn btn-ghost btn-circle">
                             <div className="indicator">
                                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v 3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
                                 </svg>
                                 <span className="badge badge-xs badge-primary indicator-item"></span>
                             </div>
-                        </button>
+                        </button> */}
                     </div>
                 </div>
             </div>
